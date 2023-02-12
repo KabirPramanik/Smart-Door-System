7469fb6f90c4746c017c598f9f700ae738d49438
Sayantan Kar <64708708+iamsayantankar@users.noreply.github.com>
2023-02-12 22:04:32 +0530

    Update create-has.php

--- backend/board/create-has.php
+++ backend/board/create-has.php
@@ -13,3 +13,5 @@ $data .= ", `gate_open_time` = '$time' ";
 $data .= ", `gate_close_by` = '' ";
 $data .= ", `gate_close_time` = '$time' ";
 $save = $conn->query("INSERT INTO `gate_auth` SET " . $data);
+
+die($haskey);
